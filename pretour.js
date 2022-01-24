0aa5616939c1a82e2cbc3b77e8fe32800c265e02
Sayujya Malkan <sayujyamalkan@gamil.com>
2022-01-24 16:07:40 +0530

    Added Feature Tour

---
+++ pretour.js
@@ -0,0 +1,9 @@
+const closeBtn = document.querySelector("#closeBtn")
+const feature = document.querySelector("#feature")
+
+closeBtn.addEventListener("click", ()=> {
+//   alert("click")
+    feature.style.display = "block";
+    feature.style.display = "none";
+
+})
